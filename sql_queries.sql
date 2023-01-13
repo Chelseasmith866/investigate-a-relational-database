b2eb49c727d69fb5d93ce88dc04e5d41a05738f6
Chelsea Smith <chelseasmith866@gmail.com>
2023-01-13 10:01:53 -0800

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,28 +1,19 @@
 QUESTION 1:
 
-SELECT
-  category.name category,
-  film.title,
-  COUNT(inventory.inventory_id) total_rentals
+SELECT category.name category, film.title, COUNT(inventory.inventory_id) total_rentals
 FROM inventory
 JOIN rental
-  ON rental.inventory_id = inventory.inventory_id
+ON rental.inventory_id = inventory.inventory_id
 JOIN film
-  ON inventory.film_id = film.film_id
+ON inventory.film_id = film.film_id
 JOIN film_category
-  ON film.film_id = film_category.film_id
+ON film.film_id = film_category.film_id
 JOIN category
-  ON film_category.category_id = category.category_id
-WHERE name IN ('Animation')
-OR name IN ('Children')
-OR name IN ('Classics')
-OR name IN ('Comedy')
-OR name IN ('Family')
-OR name IN ('Music')
-GROUP BY 1,
-         2
+ON film_category.category_id = category.category_id
+WHERE name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+GROUP BY 1, 2
 ORDER BY 3 DESC
-LIMIT 10
+LIMIT 10;
 
 
 QUESTION 2:
@@ -38,7 +29,7 @@ JOIN film
 ON film_category.film_id = film.film_id
 WHERE name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')) T1
 ORDER BY title
-LIMIT 10
+LIMIT 10;
 
 
 QUESTION 3:
@@ -54,7 +45,7 @@ JOIN film
 ON film_category.film_id = film.film_id
 WHERE name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')) T1
 GROUP BY name, t1.rental_duration
-ORDER BY name
+ORDER BY name;
 
 
 QUESTION 4:
@@ -64,8 +55,8 @@ FROM
 (SELECT store_id, rental_id,
 DATE_PART('year', rental_date) rental_year,
 DATE_PART('month', rental_date) rental_month
- FROM inventory
- JOIN rental
- ON inventory.inventory_id = rental.inventory_id)sub
- GROUP BY sub.store_id, sub.rental_month, sub.rental_year
- ORDER BY count DESC
+FROM inventory
+JOIN rental
+ON inventory.inventory_id = rental.inventory_id)sub
+GROUP BY sub.store_id, sub.rental_month, sub.rental_year
+ORDER BY count DESC;
